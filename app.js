13d72c209a5a556ea583f45e7d91d507e13b77f0
kdncode <kdncode@gmail.com>
2018-04-21 21:30:59 +0000

    added ejs and connected with server

--- app.js
+++ app.js
@@ -2,21 +2,44 @@ var express = require('express');
 var app = express();
 
 
-
-app.get('*', (req, res) => {
-    res.send('You have lost');
-});
-
 app.get('/', (req, res) => {
     res.send('hi');
 });
 
+
+// Ejs template
 app.get('/dogs', (req, res) => {
-    console.log('Someone is hacking')
-    res.send('dogs')
+    res.render('home.ejs');
+});
+
+// Connect EJS to server
+app.get('/ilike/:thing', (req, res) => {
+    var thing = req.params.thing;
+    res.render('love.ejs', {thingVar: thing})
 });
 
 
+// Routes
+app.get('/r/:id', (req, res) => {
+    var id = req.params.id;
+    res.send('Hello ' + id);
+})
+
+
+// Mutiple Routes
+app.get('/r/:sub/comments/:id/:title', (req, res) => {
+    console.log(req.params);
+    res.send('hello comments')
+})
+
+
+// 404 
+app.get('*', (req, res) => {
+    res.send('You have lost');
+});
+
+
+// Server
 app.listen(process.env.PORT, process.env.IP, () => {
     console.log('Server is running')
 });
